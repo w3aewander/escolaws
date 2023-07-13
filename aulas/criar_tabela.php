b216992b1dadebec065d5166621e482860c4c568
wanderleiscarmo <wanderlei.scarmo@educador.edu.es.gov.br>
2023-07-12 21:58:15 -0300

    Scripts de exemplo de operações com banco de dados.

--- aulas/criar_tabela.php
+++ aulas/criar_tabela.php
@@ -3,15 +3,22 @@
  * Comandos SQL no PHP.
  */
 
-$json = json_decode(file_get_contents("config.json"));
+
+$config = file_get_contents("config.json");
+$json = json_decode($config);
 
 $conexao = mysqli_connect($json->dbhost, $json->dbuser, $json->dbpass, $json->dbname);
 
+
 if ( ! $conexao ){
     die('Não foi possível conectar o banco de dados.');
-}
+} 
 
-$sql = "CREATE  TABLE IF NOT EXISTS escolaws.produtos ( id int not null auto_increment primary key, descricao varchar(50) )";
+$sql = "CREATE  TABLE 
+        IF NOT EXISTS escolaws.produtos ( 
+                          id int not null auto_increment primary key, 
+                          descricao varchar(50) 
+                    )";
 
 if ( mysqli_execute_query($conexao, $sql) ){
 
